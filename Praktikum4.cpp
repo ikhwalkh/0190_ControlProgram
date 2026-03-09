f8516e4dbd5f129b25d07859056bed10e5ba9a3b
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-09 15:37:21 +0700

    first commit

---
+++ Praktikum4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b){
+    return (a+b)/2;
+}
+
+string Status_lulus(float r){
+    if (r >= 60)
+        return "Lolos";
+    else 
+        return "Gagal";
+}
+
+int main(){
+    float nilBI,nilMT,rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    rerata = (nilBI+nilMT)/2;
+    if (rerata >= 60)
+        status = "Lolos";
+    else
+        status = "Gagal";
+    
+        cout << "Status kelulusan = "<<status<<" ,dengan nilai rerata = "<<rerata<<endl;
+}
